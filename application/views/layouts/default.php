2083fabebe52f26981969e44b4d952e9f9600c89
haryx8 <haryx8@gmail.com>
2015-05-01 16:32:47 +0700

    Enable Bootstrap on defalt layout

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -2,19 +2,43 @@
 <html lang="en">
 	<head>
 		<title>CodeIgniter - Layout</title>
+
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Blog Template for Bootstrap</title>
+		<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
 	</head>
 	
-	<body>	
+	<body>		
 		<div id="container" class="container">
-			<span id="top"></span>
-			<p>
-				<code>this is from set layout -> application/views/layouts/default.php</code>
-			</p>
-			<?=$content_for_layout ?>
-		</div>
-		
-		<div id="footer">
-			<div>{elapsed_time}Sec/ {memory_usage}</div>
-		</div>
+			<div class="row">
+    		    <div id="header" class="col-sm-12">
+    		    	<?php include('header.php');?>
+    		    </div>
+			</div>
+
+			<div class="row">
+    		    <div id="menu" class="col-sm-4">hello world</div>
+				<div id="content" class="col-sm-8">
+					<div class="jumbotron">
+						<?=$content_for_layout ?>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+    		    <div id="footer" class="col-sm-12">{elapsed_time}Sec/ {memory_usage}</div>
+			</div>
+	    </div>
+
+		<script src="assets/jquery/jquery.min.js"></script>
+    	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
 	</body>
 </html>
